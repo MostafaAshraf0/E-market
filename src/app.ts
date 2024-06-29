f97d718c8e41dd8126e30b00d4660bc77cdee3c8
MostafaAshraf0 <mostafaashraf334@gmail.com>
2024-06-29 15:02:05 +0300

    app edit

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ const fileStorage = multer.diskStorage({
 });
 
 const fileFilter = (req: Request, file: Express.Multer.File, cb: any) => {
-    if(file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg'){
+    if(file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg' || file.mimetype === 'image/webp'){
         cb(null, true);
     } else {
         cb(null, false);
